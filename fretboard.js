35ebe1ad1762dfdd84b43bb8512bb385a1bad3a0
Maxime ANDRE <maxime.andre@gadz.org>
2019-09-11 23:35:40 +0200

    Added drawing of frets and strings

--- fretboard.js
+++ fretboard.js
@@ -4,15 +4,17 @@ function Fretboard(canvas) {
 	this.defaultOptions = {
 		strings: {
 			number:6,
-			spacing: 20,
-			decreasing_width: true
+			spacing: 40,
+			width: 1,
+			increasing_width: true
 		},
 		frets: {
-			start: 0,
-			end: 12,
-			spacing: 25
+			first: 0,
+			last: 12,
+			spacing: 50,
+			width: 2
 		},
-		nut_width: 10
+		nut_width: 4
 	};
 	
 	this.options = this.defaultOptions;
@@ -22,10 +24,31 @@ function Fretboard(canvas) {
 		
 		// nut
 		ctx.beginPath();
-		ctx.lineWidth = this.defaultOptions.nut_width;
+		ctx.lineWidth = this.options.nut_width;
 		ctx.moveTo(x, y);
 		ctx.lineTo(x+this.options.strings.spacing*(this.options.strings.number-1), y);
 		ctx.stroke();
+		
+		// frets
+		for(i=0;i<=this.options.frets.last-this.options.frets.first;i++) {
+			ctx.beginPath();
+			ctx.lineWidth = this.options.frets.width;
+			ctx.moveTo(x, y+(i*this.options.frets.spacing));
+			ctx.lineTo(x+this.options.strings.spacing*(this.options.strings.number-1), y+(i*this.options.frets.spacing));
+			ctx.stroke();
+		}
+		
+		// strings
+		for(i=0;i<this.options.strings.number-1;i++) {
+			ctx.beginPath();
+			if(this.options.strings.increasing_width)
+				ctx.lineWidth = this.options.strings.number * this.options.strings.width - i;
+			else
+				ctx.lineWidth(this.options.strings.width);
+			ctx.moveTo(x+i*this.options.frets.spacing, y);
+			ctx.lineTo(x+i*this.options.frets.spacing, y+(this.options.frets.last-this.options.frets.first)*this.options.frets.spacing);
+			ctx.stroke();
+		}
 		
 		
 	}
